b81e3974fc7550b5b049277b6f8b5c4eee3ad441
cls-sigma <107411593+cls-sigma@users.noreply.github.com>
2022-11-26 09:13:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
-@app.get("/")
-def home():
-    return {"message":"Hello TutLinks.com"}
+with open("classes.txt", "r") as h:
+  clas=h.read()
+clas=clas.split(',')
+model=pickle.load(open('stack_model.pkl', 'rb'))
 
+def out(sentence):
+    predict=model.predict([sentence])
+    output=[]
+    for i, j in zip(predict[0], classes):
+      if i==1:
+        output.append(j)
+      else:
+        pass
+    return output
+
+  
 @app.post("/tags")
 async def predict(doc: str):
-    
-    return {"Tags": doc }
+    pred=out(doc)
+    return {"Tags": pred}
